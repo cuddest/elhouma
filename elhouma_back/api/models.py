2297d4317c5bb4c26f9e5be5c6385f392166ed59
cuddest <142700499+cuddest@users.noreply.github.com>
2023-09-19 00:59:25 +0100

    nc

--- elhouma_back/api/models.py
+++ elhouma_back/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Player(models.Model):
     full_name = models.CharField(max_length=35)
     age = models.IntegerField(default=0)
@@ -11,19 +12,22 @@ class Player(models.Model):
     total_titles = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
     no_of_games = models.IntegerField(default=0)
-    
-    
+
     def __str__(self):
         return self.full_name
-    
+
+
 class league(models.Model):
     name = models.CharField(max_length=50)
-    teams = models.ManyToManyField(Player, related_name='leagues')
-    
-    
+    teams = models.ManyToManyField(Player, related_name="leagues")
+
     def __str__(self):
         return self.name
-    
+
+
 class state(models.Model):
     name = models.CharField(max_length=40)
-    teams = teams = models.ManyToManyField(Player, related_name='state')
+    teams = teams = models.ManyToManyField(Player, related_name="state")
+
+    def __str__(self):
+        return self.name
